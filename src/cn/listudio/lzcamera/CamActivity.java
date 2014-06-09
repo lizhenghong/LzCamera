3a4d4c9ea8f7fe1373ee496672d085a8aa023b27
lizheng <lizheng0201@163.com>
2014-06-09 17:33:21 +0800

    can autofocus in timer

--- src/cn/listudio/lzcamera/CamActivity.java
+++ src/cn/listudio/lzcamera/CamActivity.java
@@ -1,11 +1,13 @@
 package cn.listudio.lzcamera;
 
-import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
-import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
@@ -16,6 +18,8 @@ public class CamActivity extends Activity {
 	Camera camera;
 	boolean isPreview = false;
 	SurfaceHolder surfaceHolder;
+	Handler handler;
+	Timer timer;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -45,13 +49,28 @@ public class CamActivity extends Activity {
 						camera = null;
 					}
 				}
+			});	
+			
+			
+			handler = new Handler( new Handler.Callback()
+			{
+				@Override
+				public boolean handleMessage(Message msg) {
+					// TODO Auto-generated method stub
+					
+					if(msg.what == 1)
+					{
+						camera.autoFocus(null);
+					}
+					return true;
+				}
+				
 			});
-
-		
+			
+			timer = new Timer(true);
+			timer.schedule(task,5000, 5000); 
+}
 	
-
-	}
-
 	
 	private void initCamera() {
 		if (!isPreview) {
@@ -63,17 +82,19 @@ public class CamActivity extends Activity {
 		if (camera != null && !isPreview) {
 			try {
 				Camera.Parameters parameters = camera.getParameters();
-				// 设置预览照片的大小
-			//	parameters.setPreviewSize(screenWidth, screenHeight);
+		//妈蛋不能用下面的设置，设置就报错，可能是手机不支持吧，所以lint才要强制加个try啊……
+		// 	设置预览照片的大小
+		//		parameters.setPreviewSize(100 ,200);
 				// 设置预览照片时每秒显示多少帧的最小值和最大值
-				parameters.setPreviewFpsRange(4, 10);
+		//		parameters.setPreviewFpsRange(4, 10);
 				// 设置图片格式
-				parameters.setPictureFormat(ImageFormat.JPEG);
+		//		parameters.setPictureFormat(ImageFormat.JPEG);
 				// 设置JPG照片的质量
-				parameters.set("jpeg-quality", 85);
+		//		parameters.set("jpeg-quality", 85);
 				// 设置照片的大小
-		//		parameters.setPictureSize(screenWidth, screenHeight);
+		//		parameters.setPictureSize(100, 200);
 				// 通过SurfaceView显示取景画面
+				camera.setParameters(parameters);
 				camera.setPreviewDisplay(surfaceHolder); // ②
 				// 开始预览
 				camera.startPreview(); // ③
@@ -83,4 +104,17 @@ public class CamActivity extends Activity {
 			isPreview = true;
 		}
 	}
+		
+	//自动对焦不要只放在tiemr中，还要放在手动点击，或传感器检测手机运动幅度后。
+	TimerTask task = new TimerTask(){  
+	      public void run() {  
+	      Message message = new Message();      
+	      message.what = 1;      
+	      handler.sendMessage(message);    
+	      }
+	};
+	
+	
+
+	
 }
